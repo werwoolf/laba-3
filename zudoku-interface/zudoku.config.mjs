2f3c1f57b14eda78b2a82b151c9809e1f162e4ba
Serhii Dusyk <sd1996sdsd@gmail.com>
2025-04-17 20:11:23 +0300

    init

---
+++ zudoku-interface/zudoku.config.mjs
@@ -0,0 +1,52 @@
+/** @type {import('zudoku').ZudokuConfig} */
+const config = {
+        topNavigation: [
+            {id: "docs", label: "Documentation"},
+            {id: "api", label: "API Reference"},
+            {id: "about", label: "About"},
+        ],
+        sidebar: {
+            docs: [
+                {
+                    type: "category",
+                    label: "Overview",
+                    items: ["docs/introduction", "docs/example"],
+                },
+            ],
+            about:
+                [
+                    {
+                        type: "link",
+                        label: "Installation",
+                        href: "about/installation",
+                    },
+                    {
+                        type: "link",
+                        label: "Authorization",
+                        href: "about/authorization",
+                    },
+                    {
+                        type: "link",
+                        label: "About",
+                        href: "about/about",
+                    },
+                ],
+        },
+        redirects: [{from: "/", to: "/docs/introduction"}],
+        apis:
+            {
+                type: "file",
+                input:
+                    "./apis/home-work.json",
+                navigationId:
+                    "api",
+            }
+        ,
+        docs: {
+            files: "/pages/**/*.{md,mdx}",
+        }
+        ,
+    }
+;
+
+export default config;
