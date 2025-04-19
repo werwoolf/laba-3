d278d60ca5f37f2c7825ffd3381fd928ced993cf
werwoolf <werwoolf@users.noreply.github.com>
2025-04-19 06:05:08 +0000

    deploy: 8f2c656f7c268c591526d906134672e1882c9d21

--- assets/context-DLCwaMXN-CLO1a650.js
+++
@@ -1,2 +0,0 @@
-import{r,a as i}from"./entry.client-Cc_DEcms.js";const n=r.createContext(void 0),a=({children:e,client:t})=>i.jsx(n.Provider,{value:t,children:e}),o=r.createContext(void 0),c=o.Provider,d=()=>{const e=r.useContext(o);if(!e)throw new Error("useOasConfig must be used within a OasConfigProvider");return e.config};export{d,n as i,a as u,c as x};
-//# sourceMappingURL=context-DLCwaMXN-CLO1a650.js.map
