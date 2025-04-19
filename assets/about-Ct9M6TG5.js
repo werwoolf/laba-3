fbbb1d44f4d951ee0972891227466eec44df8dbf
werwoolf <werwoolf@users.noreply.github.com>
2025-04-19 06:24:54 +0000

    deploy: a0f654605e00819a8e6d15ff21bfaf57371862d8

---
+++ assets/about-Ct9M6TG5.js
@@ -0,0 +1,6 @@
+import{L as c,j as e}from"./entry.client-DrZ7eqBe.js";const r=`Vestra Compliance Backend REST API для керування користувачами, завданнями та операціями, пов’язаними з відповідністю.
+Це версія 1.0; у майбутніх версіях можуть бути критичні зміни.
+Усі кінцеві точки вимагають автентифікації, якщо інше не вказано явно.`,a=[],i={title:"About"};function o(n){const t={info:"info",p:"p",...c(),...n.components};return e.jsx(t.info,{children:e.jsx(t.p,{children:`Vestra Compliance Backend REST API для керування користувачами, завданнями та операціями, пов’язаними з відповідністю.
+Це версія 1.0; у майбутніх версіях можуть бути критичні зміни.
+Усі кінцеві точки вимагають автентифікації, якщо інше не вказано явно.`})})}function f(n={}){const{wrapper:t}={...c(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o(n)}export{f as default,r as excerpt,i as frontmatter,a as tableOfContents};
+//# sourceMappingURL=about-Ct9M6TG5.js.map
