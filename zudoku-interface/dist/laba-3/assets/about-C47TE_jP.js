e1677d9a2143f61f13862a47287c8457fdcd4d21
Serhii Dusyk <sd1996sdsd@gmail.com>
2025-06-15 13:57:57 +0300

    deleted old build

--- zudoku-interface/dist/laba-3/assets/about-C47TE_jP.js
+++
@@ -1,2 +0,0 @@
-import{L as c,j as o}from"./entry.client-CPrFnnVk.js";const r="Цей навчальний мікро проект був розроблений Дусиком Сергієм Івановичем",i=[],a={title:"About"};function e(t){const n={info:"info",p:"p",...c(),...t.components};return o.jsx(n.info,{children:o.jsx(n.p,{children:"Цей навчальний мікро проект був розроблений Дусиком Сергієм Івановичем"})})}function f(t={}){const{wrapper:n}={...c(),...t.components};return n?o.jsx(n,{...t,children:o.jsx(e,{...t})}):e(t)}export{f as default,r as excerpt,a as frontmatter,i as tableOfContents};
-//# sourceMappingURL=about-C47TE_jP.js.map
