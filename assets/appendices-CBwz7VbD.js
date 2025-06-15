152475182a7349aabe112fe2036926d1abf3d29f
werwoolf <werwoolf@users.noreply.github.com>
2025-06-15 11:04:37 +0000

    deploy: 89ab8e7c03d9e7a5755eed38f5f3b1dd696e73de

--- assets/appendices-CBwz7VbD.js
+++ assets/appendices-CBwz7VbD.js
@@ -1,4 +1,4 @@
-import{L as i,j as e}from"./entry.client-D_wL2nO9.js";const r=[{depth:3,value:"4. Appendices",id:"4-appendices",children:[{depth:4,value:"Appendix A: Glossary",id:"appendix-a-glossary"},{depth:4,value:"Appendix B: Use Cases",id:"appendix-b-use-cases"},{depth:4,value:"Appendix C: Preliminary Schedule and Budget",id:"appendix-c-preliminary-schedule-and-budget"}]}],l=void 0;function d(s){const n={h3:"h3",h4:"h4",li:"li",strong:"strong",table:"table",tbody:"tbody",td:"td",th:"th",thead:"thead",tr:"tr",ul:"ul",...i(),...s.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.h3,{id:"4-appendices",children:"4. Appendices"}),`
+import{L as i,j as e}from"./entry.client-BUKquv9p.js";const r=[{depth:3,value:"4. Appendices",id:"4-appendices",children:[{depth:4,value:"Appendix A: Glossary",id:"appendix-a-glossary"},{depth:4,value:"Appendix B: Use Cases",id:"appendix-b-use-cases"},{depth:4,value:"Appendix C: Preliminary Schedule and Budget",id:"appendix-c-preliminary-schedule-and-budget"}]}],l=void 0;function d(s){const n={h3:"h3",h4:"h4",li:"li",strong:"strong",table:"table",tbody:"tbody",td:"td",th:"th",thead:"thead",tr:"tr",ul:"ul",...i(),...s.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.h3,{id:"4-appendices",children:"4. Appendices"}),`
 `,e.jsx(n.h4,{id:"appendix-a-glossary",children:"Appendix A: Glossary"}),`
 `,e.jsxs(n.ul,{children:[`
 `,e.jsx(n.li,{children:"See Section 1.3 for key terms."}),`
@@ -9,4 +9,4 @@ import{L as i,j as e}from"./entry.client-D_wL2nO9.js";const r=[{depth:3,value:"4
 `,e.jsxs(n.ul,{children:[`
 `,e.jsx(n.li,{children:"To be determined during project planning."}),`
 `]})]})}function a(s={}){const{wrapper:n}={...i(),...s.components};return n?e.jsx(n,{...s,children:e.jsx(d,{...s})}):d(s)}export{a as default,l as frontmatter,r as tableOfContents};
-//# sourceMappingURL=appendices-D5r5SujD.js.map
+//# sourceMappingURL=appendices-CBwz7VbD.js.map
